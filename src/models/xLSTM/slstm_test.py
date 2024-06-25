6ac01e95408ac0049a54c46786c774077f8d63bf
leifhuenderml <leifhuenderai@gmail.com>
2024-06-25 21:55:37 +0000

    save

--- src/models/xLSTM/slstm_test.py
+++ src/models/xLSTM/slstm_test.py
@@ -1,4 +1,9 @@
 from slstm import sLSTM
+import torch
 
-lstm = sLSTM(input_size=19, hidden_size=256,num_layers=1)
-print(lstm)
+lstm = sLSTM(input_size=19, hidden_size=256,)
+
+x = torch.randn((32,365,19)) 
+
+lstm(x)
+
